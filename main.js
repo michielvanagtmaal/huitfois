8cd89d8ce4ee07c9c820e4b3c93f40c8c1d1a70c
michielvanagtmaal <madlog2912@gmail.com>
2022-10-28 20:00:07 +0200

    feat: fix css, import logo

--- main.js
+++ main.js
@@ -3,28 +3,64 @@ import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources
 import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/loaders/GLTFLoader.js";
 
 import "./style.css";
-
 const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+// add orbit controls
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 10;
+controls.maxDistance = 10;
+controls.autoRotatespeed = 30;
+controls.autoRotate = true;
 
-			camera.position.z = 5;
 
-			function animate() {
-				requestAnimationFrame( animate );
+// add ambient light
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
 
-				renderer.render( scene, camera );
-			};
+const ambientLight = new THREE.AmbientLight(0x11ad3b, 1);
+ambientLight.position.set(1, -1, 1);
+scene.add(ambientLight);
 
-			animate();
+// add directional light
+const directionalLight = new THREE.DirectionalLight(0x11ad3b, 0.5);
+directionalLight.position.set(0, -10, -50);
+scene.add(directionalLight);
+
+const directionalLight2 = new THREE.DirectionalLight(0x11ad3b, 0.5);
+directionalLight2.position.set(0, 10, 50);
+scene.add(directionalLight2);
+
+
+const loader = new GLTFLoader();
+
+
+loader.load("/assets/huitfois.glb", (gltf) => {
+  const huitfois = gltf.scene;
+  huitfois.scale.set(2, 2, 2);
+  scene.add(huitfois);
+});
+
+
+
+camera.position.z = 10;
+
+
+
+function animate() {
+  
+
+  requestAnimationFrame( animate );
+  controls.update();
+	renderer.render( scene, camera );
+};
+
+	animate();
